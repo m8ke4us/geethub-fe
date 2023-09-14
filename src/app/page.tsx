a23f8b9f640ac9ee6bada826faaf941b8d6e0b02
danbiilee <danbi.db@gmail.com>
2023-09-14 23:51:27 +0900

    [GEETHUB-18] feat: 메인 페이지 기본 퍼블리싱 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 
-import Avatar from '@/components/Avatar';
 import Pagination from '@/components/Pagination';
 import Tab from '@/components/Tab';
 import CommitRankList from '@/template/CommitRankList';
@@ -15,12 +14,10 @@ const tabItems: TabProps['items'] = [
   {
     label: 'THIS YEAR',
     value: 'YEAR',
-    children: <p>this year list</p>,
   },
   {
     label: 'THIS MONTH',
     value: 'MONTH',
-    children: <p>this month list</p>,
   },
 ];
 
@@ -63,17 +60,12 @@ const commitRankItems: CommitRankListProps['items'] = [
 ];
 
 export default function Home() {
-  const brStyles = 'my-3 h-0.5 w-full bg-neutral-200';
   const [current, setCurrent] = useState(1);
 
   return (
     <Layout>
       <Tab defaultActiveValue="YEAR" items={tabItems} />
-      <div className={brStyles} />
-      <Avatar path="142975816?s=48&v=4" alt="example avatar" size={30} />
-      <div className={brStyles} />
-      <CommitRankList items={commitRankItems} />
-      <div className={brStyles} />
+      <CommitRankList className="pb-12 pt-6" items={commitRankItems} />
       <Pagination total={52} pageSize={10} current={current} onChange={setCurrent} />
     </Layout>
   );
