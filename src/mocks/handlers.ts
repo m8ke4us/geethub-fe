886247470023f485c69e1343f63288ce1b169524
danbiilee <danbi.db@gmail.com>
2023-09-15 18:50:27 +0900

    [GEETHUB-25] feat: react-query 적용

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { rest } from 'msw';
 import { commitRankItems } from '@/mocks/data/rank';
 
 export const handlers = [
-  rest.get('/api/rank', (_, res, ctx) => {
+  rest.get('/api/commit/ranks', (_, res, ctx) => {
     // TODO: 인증 여부 확인
     return res(ctx.status(200), ctx.json(commitRankItems));
   }),
